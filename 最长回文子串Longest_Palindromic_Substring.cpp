2351fa8affe8f7527025aac3bee0c5da32d61566
remain-maomao <83824640+remain-maomao@users.noreply.github.com>
2024-02-05 09:55:36 +0800

    该函数现在可以处理奇数长度的字符串和偶数长度的字符串，并且速度最快

--- 最长回文子串Longest_Palindromic_Substring.cpp
+++ 最长回文子串Longest_Palindromic_Substring.cpp
@@ -1,23 +1,23 @@
 #include<iostream>
 using namespace std;
 /**
- * 查找并返回给定字符串中最长的奇数长度回文子串。
+ * 查找并返回给定字符串中最长的回文子串。
  *
- * 此函数利用中心扩展算法寻找最长的奇数长度回文子串。它逐个检查字符串中的每个字符，
- * 将每个字符作为潜在回文子串的中心，向两边扩展比较字符，以确定最长的回文子串。
- * 该方法仅适用于寻找奇数长度的回文子串。
+ * 该函数使用中心扩展法查找最长的回文子串，分别处理奇数长度和偶数长度的情况。
+ * 对于奇数长度的子串，选择一个字符作为中心，向两边扩展；
+ * 对于偶数长度的子串，选择相邻的两个字符作为中心，向两边扩展。
+ * 函数比较这两种情况下找到的最长回文子串的长度，并返回最长的那个。
  *
- * 注意：该函数当前不处理偶数长度的回文子串。如果存在多个最长的回文子串，
- * 函数将返回第一个找到的子串。
+ * 注意：如果存在多个长度相同的最长回文子串，函数将返回在字符串中首次找到的子串。
  *
- * @param s 输入字符串，函数将在这个字符串中查找最长的奇数长度回文子串。
- * @return 返回找到的最长奇数长度回文子串。如果输入字符串为空或不存在奇数长度的回文子串，则返回空字符串。
+ * @param s 输入字符串，函数将在这个字符串中查找最长的回文子串。
+ * @return 返回找到的最长回文子串。如果输入字符串为空，或不存在回文子串，则返回空字符串。
  */
 string getLongestPalindromicString(string s) {
     int length = s.size();
     int maxI = 0; // 最长回文子串中心的索引
     int maxWing = 0; // 最长回文子串的“翼长”，即中心到边缘的长度
-    for (int i = 0; i < length - 1; i++) {
+    for (int i = 0; i < length; i++) {
         int wing = 0; // 当前中心的翼长
         while (i - wing >= 0 && i + wing <= length - 1) {
             if (s[i - wing] == s[i + wing]) {
@@ -32,7 +32,32 @@ string getLongestPalindromicString(string s) {
             maxWing = wing;
         }
     }
-    return s.substr(maxI - maxWing, 2 * maxWing + 1);
+    string longestOddPalindromicSubstr = s.substr(maxI - maxWing, 2 * maxWing + 1);
+
+    // 查找最长的偶数长度回文子串
+    maxI = maxWing = 0;
+    for (int i = 0; i < length - 1; i++) {
+        int wing = 0; // 当前中心的翼长
+        int LCenter = i;
+        int RCenter = i + 1;
+        while (LCenter - wing >= 0 && RCenter + wing <= length - 1) {
+            if (s[LCenter - wing] == s[RCenter + wing]) {
+                wing++;
+            } else {
+                break;
+            }
+        }
+        if (wing > maxWing) {
+            maxI = LCenter;
+            maxWing = wing;
+        }
+    }
+    string longestEvenPalindromicSubstr = s.substr(maxI-maxWing+1, 2* maxWing);
+
+    // 比较奇数长度和偶数长度回文子串的长度，返回最长的那个
+    return longestEvenPalindromicSubstr.size() >
+            longestOddPalindromicSubstr.size() ?
+            longestEvenPalindromicSubstr : longestOddPalindromicSubstr;
 }
 int main() {
     string s;
